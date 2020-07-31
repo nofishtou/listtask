cc8cf80520cd198eba422fb0314d17ca21fc3761
nofishtou <damnedbyhimself@gmail.com>
2020-07-31 15:01:57 +0300

    Updates

---
+++ precache-manifest.14522cd7cfee249fe4949b1cf2746669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518d60fa95600f6f64db54f243966d4c",
+    "url": "/listtask/index.html"
+  },
+  {
+    "revision": "7faa8a6aa165e7a1687b",
+    "url": "/listtask/static/css/main.e350299e.chunk.css"
+  },
+  {
+    "revision": "182b84c5a10a2627030c",
+    "url": "/listtask/static/js/2.76866422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/listtask/static/js/2.76866422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7faa8a6aa165e7a1687b",
+    "url": "/listtask/static/js/main.d4d15998.chunk.js"
+  },
+  {
+    "revision": "6510a77fc9571e3fa660",
+    "url": "/listtask/static/js/runtime-main.36adbd2a.js"
+  }
+]);
